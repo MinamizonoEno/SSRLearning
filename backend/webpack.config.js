968e2a07fbb3c345dbe6ca56a76ed6102cf175ee
MinamizonoEno <minamizono.e@gmail.com>
2024-01-27 21:12:29 +0900

    Add backend files and configuration

---
+++ backend/webpack.config.js
@@ -0,0 +1,22 @@
+// webpack.config.js
+const path = require("path");
+
+module.exports = {
+  entry: "./server/index.js",
+  target: "node",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+};
