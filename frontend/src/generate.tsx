10108dc533c564e67df6220612e13ae69011272f
Yuma Satake <satous.y@icloud.com>
2024-01-28 23:16:18 +0900

    Update build:generate script and remove unused CSS import

--- frontend/src/generate.tsx
+++ frontend/src/generate.tsx
@@ -3,13 +3,19 @@ import ReactDOMServer from "react-dom/server";
 import { App } from "./App.tsx";
 import fs from "fs";
 import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const pageString = ReactDOMServer.renderToString(<App />);
+const randomeNumber = Math.random().toString(32).substring(2);
 
 const html = `<!DOCTYPE html>
 <html lang="ja">
     <head>
     <meta charset="UTF-8" />
+    // ${randomeNumber}
     <link rel="icon" type="image/svg+xml" href="/vite.svg" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Vite + React + TS</title>
